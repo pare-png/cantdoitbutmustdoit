1a97c5c5366bab6dfc0f39df90e85c2742a2151d
Samita28 <samita.kwa@dome.tu.ac.th>
2021-12-18 17:19:08 +0700

    Change it

--- node_modules/wallpaper/source/linux/background-managers/feh.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const {commandExists, execFile} = require('../util');
-
-exports.isAvailable = () => commandExists('feh');
-
-exports.set = async imagePath => {
-	await execFile('feh', ['--bg-fill', imagePath]);
-};
--- node_modules/wallpaper/source/linux/background-managers/pcmanfm.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const {commandExists, execFile} = require('../util');
-
-exports.isAvailable = () => commandExists('pcmanfm');
-
-exports.set = async imagePath => {
-	await execFile('pcmanfm', ['--set-wallpaper', imagePath]);
-};
